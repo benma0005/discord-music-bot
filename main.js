34ebfe195c83e6069843c9d1fc0f7aa7b5f18be6
Max <59838514+benma0005@users.noreply.github.com>
2020-04-24 21:33:00 +0200

    fixed a mistake

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { Client, MessageEmbed } = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require('ytdl-core')
 const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyDpueR1IPDEIcK42itCKwEoGFOyE2HC4B0');
+const youtube = new YouTube('AIzaSyAt8wdg-YuArRHtKWK05In0VYmQwQMamaM');
 const prefix = '.'
 
 client.on('message', async message => {
@@ -17,7 +17,8 @@ client.on('message', async message => {
                 const connection = await message.member.voice.channel.join();
                 youtube.searchVideos(splitMessage[1], 1)
                     .then(results => {
-                        const plmusic = connection.play(ytdl(`${results[0].shortURL}`, { filter: 'audioonly' }))
+                        console.log(`${results[0].url}`)
+                        const plmusic = connection.play(ytdl(`${results[0].url}`, { filter: 'audioonly' }))
                         const embed = new MessageEmbed()
                         .setTitle('Lied wird abgespielt')
                         .setColor(0x03fc67)
@@ -27,7 +28,6 @@ client.on('message', async message => {
                         client.on('message', msg => {
                             if (msg.content === prefix+'pause') {
                               plmusic.pause();
-                              console.log('Music stopped'.red)
                               const embed = new MessageEmbed()
                               .setTitle('Pausiert')
                               .setColor(0x03fc67)
@@ -59,4 +59,4 @@ client.on('message', async message => {
 
 });
 
-client.login('Njk3NTM2ODEyNTg2ODkzMzYz.XqMKfA.2sd6Lsamo2ZG8Sq1Re6OQay5RZ8');
+client.login('Njk3NTM2ODEyNTg2ODkzMzYz.XqM2pg.XhJ939j3_3z0-aGDyAJvZzD2Vy0');
